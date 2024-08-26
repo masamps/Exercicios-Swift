e5e07cf936128525bf2ca8b79ae1464daab336c5
Matheus Sampaio <matheuslemos98@gmail.com>
2024-08-26 13:51:31 -0300

    exercicio-1001

---
+++ exercicio-1001/exercicio-1001/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  exercicio-1001
+//
+//  Created by Matheus Sampaio on 26/08/24.
+//
+
+import Foundation
+
+var A = readLine()!
+var B = readLine()!
+
+var soma = Int(A)! + Int(B)!
+
+print("X = \(soma)")
