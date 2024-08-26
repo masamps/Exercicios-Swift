6e06789498de279d0bae85ea69c8befd70366580
Matheus Sampaio <matheuslemos98@gmail.com>
2024-08-26 14:26:23 -0300

    exercicio 1003

---
+++ exercicio-1004/exercicio-1004/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  exercicio-1004
+//
+//  Created by Matheus Sampaio on 26/08/24.
+//
+
+import Foundation
+
+var A = readLine()!
+var B = readLine()!
+
+var prod = Int(A)! * Int(B)!
+
+print("PROD = \(prod)")
+
