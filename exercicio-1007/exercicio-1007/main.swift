f7b432b48c16b3d78b9348831a6310f57bf90f14
Matheus Sampaio <matheuslemos98@gmail.com>
2024-08-26 14:44:45 -0300

    exercicio 1007

---
+++ exercicio-1007/exercicio-1007/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  exercicio-1007
+//
+//  Created by Matheus Sampaio on 26/08/24.
+//
+
+import Foundation
+
+var A = readLine()!
+var B = readLine()!
+var C = readLine()!
+var D = readLine()!
+
+var dif = (Int(A)! * Int(B)! - Int(C)! * Int(D)!)
+
+print("DIFERENCA = \(dif)")
